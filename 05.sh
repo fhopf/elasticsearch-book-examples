7f4ccf82e2a3f3e74f682766450c7e379345e141
Florian Hopf <mail@florian-hopf.de>
2015-08-24 08:39:59 +0200

    added license and readme

--- 05.sh
+++ 05.sh
@@ -41,3 +41,57 @@ curl -XPOST "http://localhost:9200/_bulk" -d'
 # JDBC Importer to come
 
 # Attachement plugin to come
+
+# Partial update
+curl -XPOST "http://localhost:9200/example/doc/2/_update" -d'
+{
+   "doc" : {
+      "note" : "Updated!"
+   }
+}'
+
+# Retrieve document
+curl -XGET "http://localhost:9200/example/doc/2"
+
+# Index and search
+curl -XPUT "http://localhost:9200/example/doc/2" -d'
+{
+    "title": "Find me"
+}'; curl -XGET "http://localhost:9200/example/doc/_search?q=find"
+
+# Search manually
+curl -XGET "http://localhost:9200/example/doc/_search?q=find"
+
+# Set refresh rate
+curl -XPUT "http://localhost:9200/example/_settings " -d'
+{
+    "index": {
+        "refresh_interval": "60s"
+    }
+}'
+
+# Refresh manually
+curl -XPOST "http://localhost:9200/example/_refresh"
+
+# Refresh after an operation
+curl -XDELETE "http://localhost:9200/example/doc/1?refresh"
+
+# Index another document
+curl -XPOST "http://localhost:9200/example/doc/3" -d'
+{
+    "title": "Durability"
+}'
+
+# Search manually
+curl -XGET "http://localhost:9200/example/doc/_search?q=durability"
+
+# Access doc
+curl -XGET "http://localhost:9200/example/doc/3"
+
+# Flush 
+curl -XPOST "http://localhost:9200/example/_flush"
+
+# See translog stats
+curl -XGET "http://localhost:9200/example/_stats/translog?pretty"
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Examples for the german Elasticsearch book "Elasticsearch -- Ein praktischer Einstieg" available at http://dpunkt.de/buecher/5003/9783864902895-elasticsearch.html
